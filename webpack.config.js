cad56368119fb161d1c8c365741267b2566d159c
efrainlaycastillo <efrainlaycastillo@hotmail.com>
2022-01-25 22:28:30 -0500

    set new plugin to precompile sass file with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const htmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     entry: './src/index.js',
@@ -27,6 +28,14 @@ module.exports = {
                         loader: 'html-loader'
                     }
                 ]
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader"
+                ]
             }
         ]
     },
@@ -36,6 +45,16 @@ module.exports = {
                 template: './public/index.html',
                 filename: './index.html'
             }
-        )
-    ]
+        ),
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        })
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'public'),
+          },
+        compress: true,
+        port: 3000
+    }
 }
