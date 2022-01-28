f363f75094fd33ff54a43194d66cfe5f6a23d907
efrainlaycastillo <efrainlaycastillo@hotmail.com>
2022-01-27 20:37:26 -0500

    import new css and create containers

--- webpack.config.js
+++ webpack.config.js
@@ -53,7 +53,7 @@ module.exports = {
     devServer: {
         static: {
             directory: path.join(__dirname, 'public'),
-          },
+        },
         compress: true,
         port: 3000
     }
